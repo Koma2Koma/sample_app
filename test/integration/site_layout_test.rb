c6530a2bde3de191970d801305da2e0b3edf9e34
Konrad Maurer <kradjr@gmail.com>
2014-12-05 09:49:39 -0700

    Chapter 5 exercises

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,5 +8,17 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
+
+  # test "signup page title" do
+  # 	get signup_path
+  # 	assert_template 'users/new'
+  # 	assert_select "a[href=?]", root_path, count: 2
+  #   assert_select "a[href=?]", help_path
+  #   assert_select "a[href=?]", about_path
+  #   assert_select "a[href=?]", contact_path
+  # end
+
 end
