2e4bf0f4f3f219bbdd51d4fa39c7f01173900fd0
Konrad Maurer <kradjr@gmail.com>
2014-12-08 16:08:40 -0700

    Redirected successful signups and created successful flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
     else
       render 'new'
     end
@@ -25,5 +26,5 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-    
+
 end
